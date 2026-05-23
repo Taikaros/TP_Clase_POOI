4c45967e68d1ed6513dbd8d4092f0227360b57bb
Morel Garda Luca Valentino <lucaval3ntino@gmail.com>
2026-05-22 21:56:41 -0300

    termine mis puntos lol

--- TP_03/ejercicio2/src/TestEmpleado.java
+++ TP_03/ejercicio2/src/TestEmpleado.java
@@ -9,6 +9,21 @@ public class TestEmpleado {
             new EmpleadoPorHora  ("Juan", "42756097", 70000,14000,4000)
 
         };
+
+        for (int i = 0; i < misEmpleados.length; i++){
+            System.out.println("Empleado: " + misEmpleados[i].toString());
+            System.out.println("Salario: " + misEmpleados[i].calcularSalario());
+        }
+
+       EmpleadoTiempoCompleto emp1 = new EmpleadoTiempoCompleto("Carlos", "12345678", 60000, 10000);
+       EmpleadoTiempoCompleto emp2 = new EmpleadoTiempoCompleto("Jose", "12345678", 80000, 20000);
+        
+       System.out.println("¿Empleado 1 es igual a empleado 2? " + emp1.equals(emp2));
+
+       EmpleadoPorHora emp3 = new EmpleadoPorHora("Maria", "12345678", 40000, 160, 500);
+
+       System.out.println("¿El Empleado 1 es igual al Empleado 3? " + emp1.equals(emp3));
     }
+
 
 }
